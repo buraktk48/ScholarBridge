7e12720f258d42611c99ffc10b53f0c61ebebcda
owenspecter <evinimo21@gmail.com>
2026-04-29 15:38:58 +0300

    codefirst

--- ScholarBridge/Program.cs
+++ ScholarBridge/Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using ScholarBridge.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ScholarBridgeContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("baglan")));
+
+
 
 var app = builder.Build();
 
