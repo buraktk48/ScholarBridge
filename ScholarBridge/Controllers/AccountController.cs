7615f16cc417c036fbbf128e2141c2935071a1e3
owenspecter <evinimo21@gmail.com>
2026-05-11 23:00:09 +0300

    Account Controller, Login.cshtml codes

---
+++ ScholarBridge/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ScholarBridge.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
